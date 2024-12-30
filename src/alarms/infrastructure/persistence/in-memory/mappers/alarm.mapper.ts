209bdf59bb25a27d338aef5f8bf8ffa5f7017cd6
Petr Hanzl <petr.hanzl@livesport.eu>
2024-12-30 10:35:22 +0100

    init

---
+++ src/alarms/infrastructure/persistence/in-memory/mappers/alarm.mapper.ts
@@ -0,0 +1,25 @@
+import { AlarmSeverity } from "src/alarms/domain/value-objects/alarm-severity";
+import { AlarmEntity } from "../entities/alarm.entity";
+import { Alarm } from "../../../../../alarms/domain/alarm";
+
+export class AlarmMapper {
+    static toDomain(alarmEntity: AlarmEntity): Alarm {
+        const alarmSeverity = new AlarmSeverity(
+            alarmEntity.severity as 'critical' | 'low' | 'normal' | 'high'
+        );
+        const alarmModel = new Alarm(
+            alarmEntity.id,
+            alarmEntity.name,
+            alarmSeverity
+        )
+        return alarmModel;
+    }
+
+    static toPersistence(alarm: Alarm) {
+        const alarmEntity = new AlarmEntity();
+        alarmEntity.id = alarm.id;
+        alarmEntity.name = alarm.name;
+        alarmEntity.severity = alarm.severity.value;
+        return alarmEntity;
+    }
+}
